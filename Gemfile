1d61273cbffdb2273cdc1c96d119d9a7d6d872d1
danroche10 <danielroche10@gmail.com>
2021-08-10 17:38:37 +0100

    initial commit

---
+++ Gemfile
@@ -0,0 +1,6 @@
+gem 'sinatra'
+gem 'sinatra-reloader'
+gem 'rspec'
+gem 'webrick'
+gem 'capybara'
+
---
+++ app.rb
@@ -0,0 +1,17 @@
+require 'sinatra'
+require "sinatra/reloader" if development?
+
+class Battle < Sinatra::Base
+
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  get '/' do
+    "Testing infrastructure working!"
+  end
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
+end
+
