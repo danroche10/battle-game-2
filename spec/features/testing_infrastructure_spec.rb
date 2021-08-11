ba08bda8bcd6fdf59c1d27ca5cc0e91b4db21f9e
edpackard <edpackard81@gmail.com>
2021-08-11 17:21:32 +0100

    Added Attack button and page

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -13,3 +13,11 @@ feature 'See opponents hit points' do
     expect(page).to have_content "Ed hit points: 10"
   end
 end
+
+feature 'Attack a player' do
+  scenario 'P1 can attack P2 and get confirmation' do
+    sign_in_and_play
+    click_button('ATTACK!')
+    expect(page).to have_content "Dan attacked Ed"
+  end
+end
