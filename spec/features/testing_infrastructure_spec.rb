a9482d47561eaba30fdfc115ccd7074478f204b2
danroche10 <danielroche10@gmail.com>
2021-08-11 16:21:37 +0100

    added web_helpers.rb and refactored

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -2,20 +2,14 @@
 
 feature 'Player form' do
   scenario 'expects players to fill in their names and submit' do
-    visit('/')
-    fill_in('player_1', with: "Dan")
-    fill_in('player_2', with: "Ed")
-    click_button('Submit')
+    sign_in_and_play
     expect(page).to have_content "Dan is playing against Ed"
   end
 end
 
 feature 'See opponents hit points' do
   scenario 'Player 1 can see Player 2 hit points' do
-    visit('/')
-    fill_in('player_1', with: "Dan")
-    fill_in('player_2', with: "Ed")
-    click_button('Submit')
+    sign_in_and_play
     expect(page).to have_content "Ed hit points: 10"
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,11 @@
 ENV['RACK_ENV'] = 'test'
 
-# reuire our Sinatra app file
+# require our Sinatra app file
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
 require 'capybara'
 require 'capybara/rspec'
 require 'rspec'
+require 'features/web_helpers'
 
 Capybara.app = Battle
 
