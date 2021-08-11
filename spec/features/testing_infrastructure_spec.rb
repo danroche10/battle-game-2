6b5115c20abc9f3dee5442f9bdcc9194a7745aea
danroche10 <danielroche10@gmail.com>
2021-08-11 16:02:05 +0100

    View hit point complete

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -9,3 +9,13 @@ feature 'Player form' do
     expect(page).to have_content "Dan is playing against Ed"
   end
 end
+
+feature 'See opponents hit points' do
+  scenario 'Player 1 can see Player 2 hit points' do
+    visit('/')
+    fill_in('player_1', with: "Dan")
+    fill_in('player_2', with: "Ed")
+    click_button('Submit')
+    expect(page).to have_content "Ed hit points: 10"
+  end
+end
