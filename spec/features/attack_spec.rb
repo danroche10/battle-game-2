9cd0c114946fa29c45cf8356125cb80001c70297
Hotu Nga Morehu Elkington <hotunz@gmail.com>
2021-08-12 15:05:04 +0100

    Added hit point loss feature test

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -1,8 +1,16 @@
 
 feature 'Attack a player' do
+
   scenario 'P1 can attack P2 and get confirmation' do
     sign_in_and_play
     click_button('ATTACK!')
     expect(page).to have_content "Dan attacked Ed"
   end
+
+  scenario 'attack reduces hit points by 10' do
+    sign_in_and_play
+    click_button('ATTACK!')
+    expect(page).to have_content "hit points: 0"
+  end
+
 end
--- spec/features/play_spec.rb
+++ spec/features/play_spec.rb
@@ -1,6 +1,8 @@
 feature 'See opponents hit points' do
+  
   scenario 'Player 1 can see Player 2 hit points' do
     sign_in_and_play
     expect(page).to have_content "Ed hit points: 10"
   end
+
 end
