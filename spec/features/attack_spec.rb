560b16e6265fb94f1e481fca06d87d29f7b6d198
danroche10 <danielroche10@gmail.com>
2021-08-13 16:52:32 +0100

    added attack feature tests

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -1,16 +1,26 @@
 
 feature 'Attack a player' do
 
-  scenario 'P1 can attack P2 and get confirmation' do
+  before do
     sign_in_and_play
     click_button('ATTACK!')
-    expect(page).to have_content "Dan attacked Ed"
   end
 
-  scenario 'attack reduces hit points by 10' do
-    sign_in_and_play
-    click_button('ATTACK!')
-    expect(page).to have_content "hit points: 90"
+  feature "that shows who has just been attacked" do
+    scenario 'P1 can attack P2 and get confirmation' do
+      expect(page).to have_content "Dan attacked Ed"
+    end
+
+    scenario 'P2 can attack P1 and get confirmation' do
+      click_button('Back')
+      click_button('ATTACK!')
+      expect(page).to have_content "Ed attacked Dan"
+    end
   end
+
+  # scenario 'attack reduces hit points by 10' do
+  #   sign_in_and_play
+  #   expect(page).to have_content "hit points: 90"
+  # end
 
 end
--- spec/features/play_spec.rb
+++ spec/features/play_spec.rb
@@ -1,42 +1,56 @@
-feature 'See opponents hit points' do
+feature 'features tests for play page' do
 
-  scenario 'can see Player 1 hit points' do
+  before do
     sign_in_and_play
-    expect(page).to have_content "Dan hit points: 100"
   end
 
-  scenario 'Can see Player 2 hit points' do
-    sign_in_and_play
-    expect(page).to have_content "Ed hit points: 100"
+  feature 'confirm who is playing' do
+    scenario 'can see who is playing after an attack' do
+      attack_complete
+      expect(page).to have_content "Dan vs Ed"
+    end
   end
 
-  scenario 'can see Player 1 hit points after attack' do
-    sign_in_and_play
-    attack_complete
-    expect(page).to have_content "Dan hit points: 90"
+  feature 'player hit points' do
+
+    scenario 'can see Player 1 hit points' do
+      expect(page).to have_content "Dan hit points: 100"
+    end
+
+    scenario 'Can see Player 2 hit points' do
+      expect(page).to have_content "Ed hit points: 100"
+    end
+
+    scenario 'can see Player 2 hit points after attack' do
+      attack_complete
+      expect(page).to have_content "Ed hit points: 90"
+    end
+
+    scenario 'Can see Player 1 hit points after attack' do
+      2.times { attack_complete }
+      expect(page).to have_content "Dan hit points: 90"
+    end
+
   end
 
-  scenario 'Can see Player 2 hit points after attack' do
-    sign_in_and_play
-    2.times { attack_complete }
-    expect(page).to have_content "Ed hit points: 90"
+  feature 'showing whose turn it is correctly' do
+
+    scenario "Can see who's turn it is" do
+      expect(page).to have_content "Dan's turn"
+    end
+
+    scenario "Can see who's turn it is after attack" do
+      attack_complete
+      expect(page).to have_content "Ed's turn"
+    end
+
   end
 
-
-
-  scenario "Can see who's turn it is" do
-    sign_in_and_play
-    expect(page).to have_content "Dan's turn"
-  end
-
-  scenario "Can see who's turn it is after attack" do
-    sign_in_and_play
-    attack_complete
-    expect(page).to have_content "Ed's turn'"
-  end
-
-  scenario "Can attack other player and get confirmation" do
-
+  feature "attack button brings us to attack page which confirms attack" do
+    scenario "Can attack other player and get confirmation" do
+      click_button("ATTACK!")
+      expect(page).to have_content "Dan attacked Ed"
+    end
   end
 
 end
